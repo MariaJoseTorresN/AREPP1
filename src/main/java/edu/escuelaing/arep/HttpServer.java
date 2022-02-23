b4bb8dfa6e6acd932b3dd0b4dea5407d164f5124
unknown <maria.torres-n@labinfo.is.escuelaing.edu.co>
2022-02-23 16:38:47 -0500

    getPort

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -7,7 +7,7 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -22,27 +22,32 @@ public class HttpServer {
         }
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Received: " + inputLine);
-            if (!in.ready()) {
-                break;
-            }
+        String outputLine;
+        boolean running = true;
+        while (running) {
+            outputLine = "<!DOCTYPE html>"
+            + "<html>"
+            + "<head>"
+            + "<meta charset=\"UTF-8\">"
+            + "<title>Title of the document</title>\n"
+            + "</head>"
+            + "<body>"
+            + "My Web Site"
+            + "</body>"
+            + "</html>";
+            out.println(outputLine);    
         }
-        outputLine = "<!DOCTYPE html>"
-        + "<html>"
-        + "<head>"
-        + "<meta charset=\"UTF-8\">"
-        + "<title>Title of the document</title>\n"
-        + "</head>"
-        + "<body>"
-        + "My Web Site"
-        + "</body>"
-        + "</html>" + inputLine;
-        out.println(outputLine);
+        
         out.close();
         in.close();
         clientSocket.close();
         serverSocket.close();
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000;
     }
 }
